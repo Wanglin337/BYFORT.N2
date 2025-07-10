a0eb5d3aa6768930ae2d1cff497a3db39cddbf5f
Wanglin337 <bayboyrt@gmail.com>
2025-07-10 16:59:32 +0700

    Create profile.jsx

---
+++ pages/profile.jsx
@@ -0,0 +1,11 @@
+// src/pages/Profile.jsx
+import React from 'react';
+
+export default function Profile() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Halaman Profil</h1>
+      <p>Selamat datang di BYFORT!</p>
+    </div>
+  );
+}
